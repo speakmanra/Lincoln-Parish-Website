cba836a71856076d6c4a1fa5f77ac73922b1a85e
Ryan Speakman <speakmanra@gmail.com>
2019-08-01 23:35:59 -0500

    landing page mobile test

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.render("");
+});
+
 app.use(process.env.PORT || 2000, () => {
   console.log("Server Started");
 });
